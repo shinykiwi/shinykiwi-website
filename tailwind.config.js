18ef9b422a6622d4a62ac07605cd0a8ecbc5b0da
shinykiwi <kafountain5@gmail.com>
2023-01-02 22:28:32 -0500

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+  content: [
+      "./index.html",
+      "./art.html",
+      "./blog.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+}
